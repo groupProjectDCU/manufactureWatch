603f44bec79970328fa5fc2477adeded052428fb
sandeladevan <sandeladevan@gmail.com>
2025-03-17 23:38:37 +0000

    updated urls.py and views.py
    for better navigation of pages

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about.html', views.about, name='about'),
+    path('contact.html', views.contact, name='contact'),
+    path('login.html', views.login, name='login'),
+    path('register.html', views.register, name='register'),
+    path('forgotpass.html', views.forgotpass, name='forgotpass'),
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,17 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def forgotpass(request):
+    return render(request, 'forgotpass.html')
