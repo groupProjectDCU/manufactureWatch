d5ce4c4df09d12a85b37a9d707ac06d8b039449c
Sam <sam76420@gmail.com>
2025-03-18 14:34:57 +0100

    revert changes in machinery/models.py

--- machinery/models.py
+++ machinery/models.py
@@ -1,19 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Machinery(models.Model):
-    STATUS_CHOICES = [
-        ('OK', 'OK'),
-        ('Warning', 'Warning'),
-        ('Fault', 'Fault'),
-    ]
-  
-    machine_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    model = models.CharField(max_length=255)
-    description = models.TextField()
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='OK')
-    priority = models.IntegerField(default=0)
-
-    def __str__(self):
-        return f"{self.name} ({self.model})"
