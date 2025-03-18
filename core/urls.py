d4cae0b16ce2eaf16b1dcc72706d717cf6aa8925
sandeladevan <sandeladevan@gmail.com>
2025-03-18 00:14:18 +0000

    Update urls
    home.html is missed before

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home.html', views.home, name='home'),
     path('about.html', views.about, name='about'),
     path('contact.html', views.contact, name='contact'),
     path('login.html', views.login, name='login'),
