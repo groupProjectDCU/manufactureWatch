188565851a5a3644fdb43f3a78abfeda0566a2a7
Sam <sam76420@gmail.com>
2025-03-18 14:08:54 +0100

    Update database.sql
    Fix, and add sql tables

--- database.sql
+++ database.sql
@@ -3,32 +3,57 @@ USE factory_machinery;
 
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
+    first_name VARCHAR(255) UNIQUE NOT NULL,
+    last_name VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) NOT NULL
     password VARCHAR(255) NOT NULL,
-    role ENUM('Technician', 'Repair', 'Manager') DEFAULT 'Technician'
+    role ENUM('Technician', 'Repair', 'Manager','View-only') DEFAULT 'View-only'
 );
 
 CREATE TABLE machinery (
     machine_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     model VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
     status ENUM('OK', 'Warning', 'Fault') DEFAULT 'OK',
     priority INT DEFAULT 0,
-    campus VARCHAR(255) NOT NULL,
-    building VARCHAR(255) NOT NULL,
-    floor VARCHAR(255) NOT NULL,
-    room VARCHAR(255) NOT NULL,
 );
+
+CREATE TABLE machinery_assigment (
+    assignment_id INT AUTO_INCREMENT PRIMARY KEY,
+    is_active ENUM('True','False') DEFAULT 'True',
+    assigned_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    machine_id INT,
+    FOREIGN KEY (machine_id) REFERENCES machinery(machine_id) ON DELETE CASCADE,
+    );
+
+CREATE TABLE collections (
+    collections_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    );
+
+CREATE TABLE machinery_collections (
+    mach_coll_id INT AUTO_INCREMENT PRIMARY KEY,
+    collection_id INT,
+    machinery_id INT,
+    FOREIGN KEY (machine_id) REFERENCES machinery(machine_id) ON DELETE CASCADE,
+    FOREIGN KEY (collection_id) REFERENCES collections(collections_id) ON DELETE CASCADE,
+    );
 
 CREATE TABLE fault_cases (
     case_id INT AUTO_INCREMENT PRIMARY KEY,
     machine_id INT,
     created_by INT,
-    status ENUM('Open', 'Resolved') DEFAULT 'Open',
+    status ENUM('Open', 'in_progress', 'Resolved') DEFAULT 'Open',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     resolved_at TIMESTAMP NULL,
+    resolved_by INT NULL,
     FOREIGN KEY (machine_id) REFERENCES machinery(machine_id) ON DELETE CASCADE,
-    FOREIGN KEY (created_by) REFERENCES users(user_id) ON DELETE SET NULL
+    FOREIGN KEY (created_by) REFERENCES users(user_id) ON DELETE SET NULL,
+    FOREIGN KEY (resolved_by) REFERENCES users(user_id) ON DELETE SET NULL
+
 );
 
 CREATE TABLE fault_notes (
@@ -43,10 +68,14 @@ CREATE TABLE fault_notes (
 
 CREATE TABLE warnings (
     warning_id INT AUTO_INCREMENT PRIMARY KEY,
+    is_active ENUM('True','False') DEFAULT 'True',
+    resolved_at TIMESTAMP NULL,
+    resolved_by INT NULL,
     machine_id INT,
     message VARCHAR(255) NOT NULL,
     added_by INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (machine_id) REFERENCES machinery(machine_id) ON DELETE CASCADE,
-    FOREIGN KEY (added_by) REFERENCES users(user_id) ON DELETE SET NULL
+    FOREIGN KEY (added_by) REFERENCES users(user_id) ON DELETE SET NULL,
+    FOREIGN KEY (resolved_by) REFERENCES users(user_id) ON DELETE SET NULL
 );
