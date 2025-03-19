19bbff1de8c30b68e0196a63cd7c2c77ce0f3c1b
Sam <sam76420@gmail.com>
2025-03-19 18:04:14 +0100

    Update models.py
    add collections

--- machinery/models.py
+++ machinery/models.py
@@ -17,3 +17,20 @@ class Machinery(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.model})"
+        
+class Collection(models.Model):
+    collection_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class MachineryCollection(models.Model):
+    mach_coll_id = models.AutoField(primary_key=True)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    machinery = models.ForeignKey(Machinery, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.machinery.name} in {self.collection.name}"
