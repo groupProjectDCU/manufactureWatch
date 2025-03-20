f1224da9ee10ae84e5cd8d200bdf9340cdd5200e
kylo-539 <kas.11.20144@gmail.com>
2025-03-20 20:59:15 +0000

    Update models.py
    created ROLES field and overriding __str__

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):  #django's built-in user authentication
+    ROLES = [
+        ('Technician', 'Technician'),
+        ('Repair', 'Repair'),
+        ('Manager', 'Manager'),
+        ('View-only', 'View-only'),
+    ]
+
+    role = models.CharField(max_length=20, choices=ROLES, default='View-only')
+
+    def __str__(self):
+        return f"{self.username} - {self.role}"
