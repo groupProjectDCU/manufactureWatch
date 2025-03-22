bc373bfa2b666c9e24086e29926ba78ee3a8bc6e
kylo-539 <kas.11.20144@gmail.com>
2025-03-22 00:46:48 +0000

    Update models.py
    removed attributes already in the class and made roles capitals to fit in with convention

--- accounts/models.py
+++ accounts/models.py
@@ -2,15 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):  #django's built-in user authentication - abstract user includes email & password
-    user_id = models.BigAutoField(primary_key=True) #explicit primary key
-    first_name = models.CharField(max_length=255, unique=True)    #can change if necessary
-    last_name = models.CharField(max_length=255, unique=True)  #ditto
-    
     ROLES = [
-        ('Technician', 'Technician'),
-        ('Repair', 'Repair'),
-        ('Manager', 'Manager'),
-        ('View-only', 'View-only'),
+        ('TECHNICIAN', 'Technician'),
+        ('REPAIR', 'Repair'),
+        ('MANAGER', 'Manager'),
+        ('VIEW-ONLY', 'View-only'),
     ]
 
     role = models.CharField(max_length=20, choices=ROLES, default='View-only')
